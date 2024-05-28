78212c10746aadf31e057a0547562d7abb7d32e1
Stefan Veljkovic <stef.fac150@gmail.com>
2024-05-29 01:41:58 +0200

    Created search with OR condition (should)

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+from pprint import pprint
 import re
 from flask import Flask, render_template, request
+from search import Search
 
 app = Flask(__name__)
-
+es = Search()
 
 @app.get('/')
 def index():
@@ -12,10 +14,38 @@ def index():
 @app.post('/')
 def handle_search():
     query = request.form.get('query', '')
-    return render_template(
-        'index.html', query=query, results=[], from_=0, total=0)
+    results = es.search(
+        query={
+           'bool':{
+                'should':{
+                    'match':{
+                        "contents":{
+                            'query': query
+                        }
+                    },
+                    'match':{
+                        "title":{
+                            'query': query
+                        }
+                    }
+                }
+            }
+        }
+    )
+    pprint(results)
+    return render_template('index.html', results=results['hits']['hits'],
+                           query=query, from_=0,
+                           total=results['hits']['total']['value'])
 
 
 @app.get('/document/<id>')
 def get_document(id):
     return 'Document not found'
+
+@app.cli.command()
+def make_index():
+    es.build_index()
+
+@app.cli.command()
+def add_document():
+    es.create_document()
--- search.py
+++ search.py
@@ -5,18 +5,36 @@ import time
 
 from dotenv import load_dotenv
 from elasticsearch import Elasticsearch
+from flask import app
 
 load_dotenv()
 
-CERT_FINGERPINT = "0F:4D:33:CB:A3:F4:74:B3:AB:EA:D1:48:D1:A7:CA:4B:CA:8C:38:0C:03:B7:35:B4:3F:7E:4F:54:E6:B5:41:94"
-ELASTIC_PASSWORD= "y0_8ppm2XlshcSoqizyT"
+ELASTIC_PASSWORD= "0_TtDyV0V7lW3U*yM+Mq"
 class Search:
     def __init__(self):
         self.es = Elasticsearch(
             'https://localhost:9200',
-            ssl_assert_fingerprint=("bff1685089a4264bf03971844639040101aa51424e5f295cf3a51b1fb7c824f3"),
+            ssl_assert_fingerprint=("4963d0ec08ff37859e0211405a5b5642330307c794d4dc27074274907113328a"),
             basic_auth=("elastic",ELASTIC_PASSWORD),
             http_compress=True) # <-- connection options need to be added here
         client_info = self.es.info()
         print('Connected to Elasticsearch!')
         pprint(client_info.body)
+
+    def build_index(self):
+        self.es.indices.create(index="my_documents")
+
+    def search(self, **query_args):
+        return self.es.search(index='my_documents', **query_args)
+    
+    def print_message():
+        print("First message is printed")
+
+    def create_document(self):
+        document = {
+            'title': 'Work From Home Policy',
+            'contents': 'The purpose of this full-time work-from-home policy is...',
+            'created_on': '2023-11-02',
+        }
+        response = self.es.index(index='my_documents', body=document)
+        print(response['_id'])
